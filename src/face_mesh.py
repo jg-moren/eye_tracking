4f4d1d628a4c7612966d0ee3115f9e86f3500b3b
jg-moren <jgpmoren@gmail.com>
2025-03-12 21:22:13 -0300

    init

---
+++ src/face_mesh.py
@@ -0,0 +1,48 @@
+import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_face_mesh = mp.solutions.face_mesh
+
+class FaceMesh:
+
+
+    def __init__(self):
+
+
+        self.face_mesh = mp_face_mesh.FaceMesh(
+            max_num_faces=1,
+            refine_landmarks=True,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5)
+        
+        self.result = []
+
+
+    def detect(self, image):
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        self.result = self.face_mesh.process(image)
+        
+        
+
+    def drawMask(self, image):
+        #image.flags.writeable = True
+        
+        if self.result.multi_face_landmarks:
+            for face_landmarks in self.result.multi_face_landmarks:
+                con = frozenset().union(*[
+                    mp_face_mesh.FACEMESH_RIGHT_IRIS, 
+                    mp_face_mesh.FACEMESH_LEFT_IRIS,
+                    mp_face_mesh.FACEMESH_RIGHT_EYE,
+                    mp_face_mesh.FACEMESH_LEFT_EYE
+                ])
+                mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=con,#mp_face_mesh.FACEMESH_RIGHT_IRIS,
+                    landmark_drawing_spec=None
+                )
+        return image
+       
---
+++ src/main.py
@@ -0,0 +1,63 @@
+import cv2
+import mediapipe as mp
+import face_mesh as fm
+
+FRAME_SIZE = (640, 480)
+#FRAME_SIZE = (320, 240)
+#FRAME_SIZE = (800, 800)
+
+
+def getCapture( source ):
+    capture = cv2.VideoCapture( source )
+
+    capture.set(cv2.CAP_PROP_FPS, 30.0)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_SIZE[0])
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_SIZE[1])
+
+    width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(capture.get(cv2.CAP_PROP_FPS))
+
+    print(f"Source({source}), fps: {fps}, resolution: {width} x {height}")
+
+    return capture
+
+
+def getFrame( cap ):
+    ok, frame = cap.read()
+    frame = cv2.resize(frame, (FRAME_SIZE), interpolation=cv2.INTER_LANCZOS4)
+    return frame    
+
+
+def showFrame( frame ):
+    frame = cv2.resize(frame, (FRAME_SIZE), interpolation=cv2.INTER_LANCZOS4)
+    cv2.imshow('img',frame)
+
+
+def main():
+
+    #cap = getCapture("temp/examaple.mp4")
+    cap = getCapture(2)
+
+    detection = fm.FaceMesh()
+
+    while( cap.isOpened() ):
+        
+        frame = getFrame(cap)
+
+        detection.detect(frame)
+
+        detection.drawMask(frame)
+
+        frame = cv2.flip(frame,1)
+        
+        showFrame(frame)
+
+
+        if ( cv2.waitKey(5) & 0xFF == ord('q') ):
+            cap.release()
+        
+
+
+if __name__ == '__main__':
+    main()
