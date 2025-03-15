29453c92384290153e0e1f4561490311380bf5cf
jg-moren <jgpmoren@gmail.com>
2025-03-15 19:49:43 -0300

    build: classe de video

--- src/main.py
+++ src/main.py
@@ -1,38 +1,8 @@
-import cv2
 import face_mesh as fm
+import video
 import mat
+import cv2
 
-FRAME_SIZE = (640, 480)
-#FRAME_SIZE = (320, 240)
-#FRAME_SIZE = (800, 800)
-
-
-def getCapture( source ):
-    capture = cv2.VideoCapture( source )
-
-    #capture.set(cv2.CAP_PROP_FPS, 30.0)
-    #capture.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_SIZE[0])
-    #capture.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_SIZE[1])
-
-    width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(capture.get(cv2.CAP_PROP_FPS))
-
-    print(f"Source({source}), fps: {fps}, resolution: {width} x {height}")
-
-    return capture
-
-
-def getFrame( cap ):
-    ok, frame = cap.read()
-    frame = cv2.resize(frame, (FRAME_SIZE), interpolation=cv2.INTER_LANCZOS4)
-    return frame    
-
-
-def showFrame( frame ):
-    frame = cv2.resize(frame, (FRAME_SIZE), interpolation=cv2.INTER_LANCZOS4)
-    frame = cv2.flip(frame,1)
-    cv2.imshow('img',frame)
 
 def getEye( detection, frame, num_eye_in, num_eye_out, num_eye_pupil ):
     detection.drawPoint(frame, num_eye_in)
@@ -49,14 +19,13 @@ def getEye( detection, frame, num_eye_in, num_eye_out, num_eye_pupil ):
 
 def main():
 
-    #cap = getCapture("temp/examaple.mp4")
-    cap = getCapture(2)
-
+    #cap = video.Video("temp/examaple.mp4")
+    cap = video.Video(2)
     detection = fm.FaceMesh()
 
     while( cap.isOpened() ):
         
-        frame = getFrame(cap)
+        frame = cap.getFrame()
 
         detection.detect(frame)
 
@@ -70,17 +39,17 @@ def main():
 
         eye_right_center *= 200
         eye_right_center.y += 100
-        eye_right_center.x += 100
+        eye_right_center.x += 200
 
 
-        cv2.circle(frame, (100,100), 0, (255,0,0), 100)
-        cv2.circle(frame, (200,100), 0, (255,0,0), 100)
+        cv2.circle(frame, (100,100), 0, (255,255,255), 150)
+        cv2.circle(frame, (300,100), 0, (255,255,255), 150)
         
-        cv2.circle(frame, eye_left_center.get(), 0, (255,255,255), 20)
-        cv2.circle(frame, eye_right_center.get(), 0, (255,255,255), 20)
-
+        cv2.circle(frame, eye_left_center.get(), 0, (255,0,0), 40)
+        cv2.circle(frame, eye_right_center.get(), 0, (255,0,0), 40)
         
-        showFrame(frame)
+        
+        video.showFrame(frame)
 
 
         if ( cv2.waitKey(5) & 0xFF == ord('q') ):
---
+++ src/video.py
@@ -0,0 +1,39 @@
+import cv2
+
+FRAME_SIZE = (640, 480)
+#FRAME_SIZE = (320, 240)
+#FRAME_SIZE = (800, 800)
+
+def showFrame( frame ):
+    frame = cv2.resize(frame, (FRAME_SIZE), interpolation=cv2.INTER_LANCZOS4)
+    frame = cv2.flip(frame,1)
+    cv2.imshow('img',frame)
+
+class Video:
+
+    def __init__(self, source):
+        self.capture = cv2.VideoCapture( source )
+
+        #capture.set(cv2.CAP_PROP_FPS, 30.0)
+        #capture.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_SIZE[0])
+        #capture.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_SIZE[1])
+
+        width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(self.capture.get(cv2.CAP_PROP_FPS))
+
+        print(f"Source({source}), fps: {fps}, resolution: {width} x {height}")
+
+
+    def getFrame( self ):
+        ok, frame = self.capture.read()
+        frame = cv2.resize(frame, (FRAME_SIZE), interpolation=cv2.INTER_LANCZOS4)
+        return frame    
+
+    
+    def isOpened(self):
+        return self.capture.isOpened()
+    
+
+    def release(self):
+        self.capture.release()
